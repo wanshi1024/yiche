f2629e9434f8cf4c911fb698d1390f32c2e5f5d2
wanshi1024 <2942504702@qq.com>
2020-04-08 17:30:10 +0800

    ws

--- db1.js
+++ db1.js
@@ -1,22 +1,6 @@
 document.body.style.backgroundColor = "#E2E7D1";
-
-var zs = document.querySelector('.w450').childNodes;
-for (let i = 0; i < zs.length; i++) {
-    if (zs[i].nodeName == 'DIV')
-        zs[i].style.display = 'none';
-}
-
-document.onclick = function () {
+document.querySelector("#app").style.display = "none";
+document.onclick = () => {
     let hrefStr = localStorage.getItem('hrefStr');
     window.open(hrefStr)
 }
-
-let showCont = document.querySelectorAll('.show-cont');
-for (let i = 0; i < showCont.length; i++) {
-    showCont[i].style.display = 'none'
-}
-
-let pages = document.querySelectorAll(".pages")
-pages.forEach(e => {
-    e.style.display = "none"
-});
